14a30d6c498633b012c07e73f23385111c5ea1d0
dimanne <causal.consistency@proton.me>
2024-05-06 18:45:19 +0100

    Run proxy from own async_main

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,3 +1,5 @@
+use pingora::services::Service;
+
 pub fn init_logger(log_level: &str) {
    let mut builder = env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(log_level));
    builder.format_timestamp_micros();
@@ -99,20 +101,46 @@ impl pingora_core::apps::HttpServerApp for MyProxy {
    fn http_cleanup(&self) { self.shutdown.notify_waiters(); }
 }
 
-// #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-   init_logger(&"info");
-   let opt = pingora_core::server::configuration::Opt { upgrade:   false,
-                                                        daemon:    false,
-                                                        nocapture: false,
-                                                        test:      false,
-                                                        conf:      None, };
-   let mut server = pingora_core::server::Server::new(Some(opt)).unwrap();
-   server.bootstrap();
+
+
+async fn async_main(shutdown_recv: pingora_core::server::ShutdownWatch) {
+   let fds = pingora::server::Fds::new();
+   let fds = Some(std::sync::Arc::new(tokio::sync::Mutex::new(fds)));
+
    let mut proxy =
       pingora_core::services::listening::Service::new("Pingora HTTP Proxy Service".into(), MyProxy::new());
    proxy.add_tcp("0.0.0.0:8080");
-   server.add_service(proxy);
-   server.run_forever();
+
+   let mut service = proxy;
+
+   service.start_service(fds, shutdown_recv).await;
+   log::info!("service exited.")
+}
+
+// #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+   init_logger(&"info");
+   //    let opt = pingora_core::server::configuration::Opt { upgrade:   false,
+   //                                                         daemon:    false,
+   //                                                         nocapture: false,
+   //                                                         test:      false,
+   //                                                         conf:      None, };
+
+   // let mut server = pingora_core::server::Server::new(Some(opt)).unwrap();
+   let (_tx, shutdown_recv) = tokio::sync::watch::channel(false);
+   // let configuration = opt.conf.as_ref().map_or_else(|| { pingora_core::server::configuration::ServerConf::new_with_opt_override(&opt).unwrap() },
+   //                                                           |_| { pingora_core::server::configuration::ServerConf::load_yaml_with_opt_override(&opt).unwrap() });
+   // let configuration = std::sync::Arc::new(configuration);
+   // let options = opt;
+   // server.bootstrap();
+   // let mut services: Vec<Box<dyn pingora_core::services::Service>> = Default::default();
+   // server.add_service(proxy);
+   // services.push(Box::new(proxy));
+   // server.run_forever();
+   // let conf = configuration.as_ref();
+   log::info!("Server starting");
+   let service_runtime = pingora_runtime::Runtime::new_steal(4, "Proxy");
+   service_runtime.get_handle()
+                  .block_on(async move { async_main(shutdown_recv.clone()).await });
    Ok(())
 }
